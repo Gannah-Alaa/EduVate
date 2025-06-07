20814ba58f0dd10e9f97ee067e0ec80ce434fdef
samaawaleedd <samawaleed72003@gmail.com>
2025-06-07 12:51:25 +0300

    Update Profile.php

--- Student/Profile.php
+++ Student/Profile.php
@@ -92,6 +92,8 @@ while ($row = mysqli_fetch_assoc($scheduleRun)) {
 </head>
 <body>
 <header>
+    <!-- start navbar -->
+
     <nav class="navbar navbar-expand-lg">
         <div class="container-fluid ms-4">
             <a class="navbar-brand logo" href="#home">EduVate</a>
@@ -101,24 +103,24 @@ while ($row = mysqli_fetch_assoc($scheduleRun)) {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0 me-4">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#home">Home</a>
+                        <a class="nav-link" aria-current="page" href="home.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#aboutus-container">AboutUs</a>
+                        <a class="nav-link" aria-current="page" href="home.php#aboutus-container">AboutUs</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#events">Events</a>
+                        <a class="nav-link" aria-current="page" href="home.php#events">Events</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#contactus">ContactUs</a>
+                        <a class="nav-link" aria-current="page" href="home.php#contactus">ContactUs</a>
                     </li>
                     <?php if(isset($_SESSION['StudentID'])): ?>
                         <li class="nav-item">
                             <a class="nav-link" aria-current="page" href="Profile.php">Profile</a>
                         </li>
-                        <!-- <li class="nav-item">
+                        <li class="nav-item">
                             <a class="nav-link" aria-current="page" href="FinalReport.php">Marks</a>
-                        </li> -->
+                        </li>
                         <li class="nav-item">
                             <a class="nav-link" aria-current="page" href="Profile.php#classes">Subjects</a>
                         </li>
@@ -135,7 +137,6 @@ while ($row = mysqli_fetch_assoc($scheduleRun)) {
         </div>
     </nav>
     <!-- end navbar -->
-
 
     <div class="container bg- min-vh-100 ">
 
