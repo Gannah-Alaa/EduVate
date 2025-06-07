1e9124bf38afd481fe60978134c0f691e245c09b
samaawaleedd <samawaleed72003@gmail.com>
2025-06-07 12:45:31 +0300

    Update subMarks.php

--- Student/subMarks.php
+++ Student/subMarks.php
@@ -259,8 +259,10 @@ $classAvg = mysqli_fetch_assoc($classAvgResult)['class_avg'];
     </style>
 </head>
 <body class="bg-light">
-    <nav class="navbar navbar-expand-lg fixed-top">
-        <div class="container-fluid">
+
+    <!-- start navbar -->
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid ms-4">
             <a class="navbar-brand logo" href="#home">EduVate</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -268,22 +270,40 @@ $classAvg = mysqli_fetch_assoc($classAvgResult)['class_avg'];
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0 me-4">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="home.php">Home</a>
+                        <a class="nav-link" aria-current="page" href="#home">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="profile.php">Profile</a>
+                        <a class="nav-link" aria-current="page" href="#aboutus-container">AboutUs</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="finalReport.php">Marks</a>
+                        <a class="nav-link" aria-current="page" href="#events">Events</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="Profile.php#classes">Subjects</a>
+                        <a class="nav-link" aria-current="page" href="#contactus">ContactUs</a>
                     </li>
+                    <?php if(isset($_SESSION['StudentID'])): ?>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="Profile.php">Profile</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="FinalReport.php">Report</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="Profile.php#classes">Subjects</a>
+                        </li>
+                    <?php endif; ?>
                 </ul>
+                <?php if(isset($_SESSION['StudentID'])): ?>
+                    <form method="post">
+                        <button type="submit" name="logout" class="btn btn-outline-warning ms-3 me-4 ">Logout</button>
+                    </form>
+                <?php else: ?>
+                    <button type="button" class="btn btn-outline-warning ms-3 me-4 "><a href="login.php">Login</a></button>
+                <?php endif; ?>
             </div>
         </div>
     </nav>
-    <!-- end navbar --> <br> <br><br><br><br>
+    <!-- end navbar --> <br> <br><br> <br>
 
     <div class="container mt-4">
         <div class="row">
